4ec0ad8331fa4db340ced0b3e354210748b1f834
badr <badr@lionsgeek.ma>
2023-10-31 12:49:20 +0100

    test

---
+++ script.sh
@@ -0,0 +1,24 @@
+echo -n "Folder name : "
+
+read folder
+
+mkdir -p /home/w/Desktop/$folder/public/img /home/w/Desktop/$folder/public/pages /home/w/Desktop/$folder/public/css
+touch /home/w/Desktop/$folder/index.html
+
+echo "mbrook!!!!"
+
+
+
+
+# echo -n "Your Name : "
+
+# read nom
+
+# echo -n "Your Age : "
+
+# read age
+
+# echo "your name" $nom "your age" $age
+
+
+
