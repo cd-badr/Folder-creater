3be5642c286990986297c6eda957ab341367b096
badr <badr@lionsgeek.ma>
2023-10-31 12:51:53 +0100

    add new comment

--- script.sh
+++ script.sh
@@ -6,3 +6,5 @@ mkdir -p /home/w/Desktop/$folder/public/img /home/w/Desktop/$folder/public/pages
 touch /home/w/Desktop/$folder/index.html
 
 echo "mbrook!!!!"
+
+# Replace /path/to/your/script.sh with the actual path to your script.
